f92a0a90a9f568bb110c2040a53da17cca22eece
eymeric <hatchchien@protonmail.com>
2025-11-20 08:48:03 +0100

    init 2

---
+++ demoSBT/src/test/java/local/epul4a/demosbt/DemoSbtApplicationTests.java
@@ -0,0 +1,13 @@
+package local.epul4a.demosbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
